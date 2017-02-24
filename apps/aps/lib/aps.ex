3aeacb5fca4ce1baa9252ecda582286e97031a55
David Heyman <d.heyman256@gmail.com>
2017-02-24 15:51:11 -0500

    Initial mix of aps, the Abstract Positioning System

---
+++ apps/aps/lib/aps.ex
@@ -0,0 +1,18 @@
+defmodule APS do
+  @moduledoc """
+  Documentation for APS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> APS.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/aps/test/aps_test.exs
@@ -0,0 +1,8 @@
+defmodule APSTest do
+  use ExUnit.Case
+  doctest APS
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
