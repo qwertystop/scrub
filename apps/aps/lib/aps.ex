6b7da8a3238c8f387ba8390703d57f0fc55221f5
David Heyman <dheyman@fastmail.com>
2017-02-27 22:25:45 -0500

    Move most functions out of macro. Fix syntax errors in them that weren't being flagged while they were in there.

--- apps/aps/lib/aps.ex
+++ apps/aps/lib/aps.ex
@@ -47,88 +47,120 @@ defmodule APS do
   defmacro __using__(opts) do
     quote do
       use GenServer
+      import APS
       # Public API of Zones
+      # This part is configured by argument to __using__
       @doc """
       Starts up the Zone with default arguments
       """
       def start_link do
         GenServer.start_link(__MODULE__, unquote(opts))
       end
-
-      @doc """
-      Add a new object to the specified Zone.
-      (specify a Zone by pid or by name)
-      """
-      def add_object(zone, tags, module, args, options \\ []) do
-        GenServer.cast(zone, {:addobj, {tags, module, args, options}})
-      end
-
-
-      ## GenServer callbacks
-      @doc """
-      Initialize the Zone.
-      Argument must be a tuple containing:
-      - object_setup
-
-      object_setup is a list of object parameter tuples.
-      An object parameter tuple is {tag_list, module, args, opts}
-      tag_list is a list of symbols used to tag the specific object.
-      The others are the arguments to Agent.start_link/4 to create the object,
-      where the function called is :init
-      """
-      def init({object_setup}) do
-        {objects, tagmap} = start_objects(object_setup)
-        {ok, %{objects: objects, tags: tagmap}, :hibernate}
-      end
-
-      @doc """
-      Add an object to this.
-      """
-      def handle_cast({:addobj, {tags, module, args, opts}=params}, state) do
-        {objlist, taglist} = add_obj(state.objects, state.tags, params)
-        {:noreply, %{state |
-            :objects => objlist,
-            :tags => taglist}}
-      end
-
-      ## Private functions
-
-      # Starts a new object,
-      # returns updated taglist and objlist
-      defp add_obj(objlist, tagmap, {tags, module, args, opts}) do
-        {:ok, pid} = Agent.start_link(module, :init, args, opts)
-        {[pid | objlist], update_tags(tagmap, tags, pid)}
-      end
-
-      # Start all the objects and collect their tags
-      defp start_objects([params | rest]=obj_setup_list,
-          pids \\ [], tagmap \\ %{}) do
-        {objlist, tagmap} = add_obj(pids, tagmap, params)
-        start_objects(rest, objlist, tagmap)
-      end
-
-      # base case
-      defp start_objects([], pids \\ [], tagmap \\ %{}) do
-        {pids, tagmap}
-      end
-
-      # Add item to all new tags, add them to existing
-      defp update_tags(existing, new, item) do
-        [first | rest] = new
-        case existing do
-          # If first is there, prepend new item
-          %{^first => vals} ->
-            result = %{existing | first => [item | existing[first]]}
-            # else make a new list
-            _ -> result = %{existing | first => [item]}
-          end
-          update_tags(result, rest, item)
-        end
-
-      # base case
-      defp update_tags(existing, [], _item) do
-        existing
-      end
     end
+  end
+
+  # Public API of Zones
+  @doc """
+  Add a new object to the specified Zone.
+  (specify a Zone by pid or by name)
+  """
+  def add_object(zone, tags, module, args, options \\ []) do
+    GenServer.cast(zone, {:addobj, {tags, module, args, options}})
+  end
+
+  @doc """
+  Return the list of tags in the given zone
+  """
+  def show_tags(zone) do
+    GenServer.call(zone, :showtags)
+  end
+
+  @doc """
+  Returns the  list of objects with the given tag in the zone.
+  Returns an empty list if there are no such objects.
+  """
+  def find_tagged(zone, tag) do
+    GenServer.call(zone, {:findtagged, tag})
+  end
+
+  ## GenServer callbacks
+
+  @doc """
+  Initialize the Zone.
+  Argument must be a tuple containing:
+  - object_setup
+
+  object_setup is a list of object parameter tuples.
+  An object parameter tuple is {tag_list, module, args, opts}
+  tag_list is a list of symbols used to tag the specific object.
+  The others are the arguments to Agent.start_link/4 to create the object,
+  where the function called is :init
+  """
+  def init({object_setup}) do
+    {objects, tagmap} = start_objects(object_setup)
+    {:ok, %{objects: objects, tags: tagmap}, :hibernate}
+  end
+
+  # Calls
+
+  def handle_call(:showtags, state) do
+    state.tags
+  end
+
+  def handle_call({:findtagged, tag}, state) do
+    case state.tags do
+      %{^tag => val} -> val
+      _ -> []
+    end
+  end
+
+  # Casts
+
+  @doc """
+  Add an object to this.
+  """
+  def handle_cast({:addobj, {_tags, _module, _args, _opts}=params}, state) do
+    {objlist, taglist} = add_obj(state.objects, state.tags, params)
+    {:noreply, %{state |
+        :objects => objlist,
+        :tags => taglist}}
+  end
+
+  ## Private functions
+
+  # Starts a new object,
+  # returns updated taglist and objlist
+  defp add_obj(objlist, tagmap, {tags, module, args, opts}) do
+    {:ok, pid} = Agent.start_link(module, :init, args, opts)
+    {[pid | objlist], update_tags(tagmap, tags, pid)}
+  end
+
+  # Start all the objects and collect their tags
+  defp start_objects(obj_setup_list, pids \\ [], tagmap \\ %{})
+  defp start_objects([params | rest], pids, tagmap) do
+    {objlist, tagmap} = add_obj(pids, tagmap, params)
+    start_objects(rest, objlist, tagmap)
+  end
+
+  # base case
+  defp start_objects([], pids, tagmap) do
+    {pids, tagmap}
+  end
+
+  # Add item to all new tags, add them to existing
+  defp update_tags(existing, [tag | rest], item) do
+    case existing do
+      # If first is there, prepend new item
+      %{^tag => vals} ->
+        result = %{existing | tag => [item | vals]}
+        # else make a new list
+        _ -> result = %{existing | tag => [item]}
+      end
+      update_tags(result, rest, item)
+    end
+
+  # base case
+  defp update_tags(existing, [], _item) do
+    existing
   end
 end
